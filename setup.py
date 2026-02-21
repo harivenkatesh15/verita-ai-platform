7b1da32171645b665ae4b1b0b82a6273b2b9f340
Hari <harivenkatesh1505@gmail.com>
2026-02-21 23:50:14 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_chatbot',
+    version='0.1.0',
+    author='Hari',
+    author_email='harivenkatesh1505@gmail.com',
+    description='A medical chatbot application',
+    packages=find_packages(),
+    install_requires=[]
+)
