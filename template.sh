1d945efb56fc988dff6cc6d102e8715c9119bd12
Hari <harivenkatesh1505@gmail.com>
2026-02-21 23:23:44 +0530

    Project structure ha been created

---
+++ template.sh
@@ -0,0 +1,15 @@
+# Create the necessary directories for the medical chatbot project
+mkdir -p src
+mkdir -p research
+
+# Creating Files for the project
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully!"
